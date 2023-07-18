6d0b81a9893650760815ee52f8ce154f399eea1f
worksofallwin <princeallwin199@gmail.com>
2023-07-18 19:20:06 +0530

    Add verification to deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config()
+require("dotenv").config();
+require("@nomicfoundation/hardhat-verify");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
 const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -12,10 +14,13 @@ module.exports = {
         sepolia: {
             url: SEPOLIA_RPC_URL,
             accounts: [SEPOLIA_PRIVATE_KEY],
-            chainId: 11155111
-        }
+            chainId: 11155111,
+        },
     },
     solidity: "0.8.18",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
 };
 
 //yarn hardhat run scripts/deploy.js --network sepolia
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,47 @@
-const {ethers} = require("hardhat");
+const { ethers, run, network } = require("hardhat");
+require("dotenv").config();
 
 async function main() {
-
     console.log("Deploying Contract............");
-
+    /////////////////////////////
     const simpleStorage = await ethers.deployContract("SimpleStorage");
     await simpleStorage.waitForDeployment();
-    
+    const transactionReceipt = await simpleStorage.deploymentTransaction();
+
+    // console.log("Receipt", await simpleStorage.deploymentTransaction().wait(2));
+    /////////////////////////////
     console.log(`Deployed contract to: ${simpleStorage.target}`);
 
-};
+    // //////////////  Verification   ///////////////////
+    // if (network.config.chainId !== 31337 && process.env.ETHERSCAN_API_KEY) {
+    //     console.log(`Waiting for Block Confirmations......`);
+    //     await verify(simpleStorage.target, []);
+    // }
+
+    const currentValue = await simpleStorage.showFavouriteNumber();
+    console.log(`Current Value ${currentValue}`);
+    const transactionResponse = await simpleStorage.store("22");
+    await transactionResponse.wait(1);
+    const updatedValue = await simpleStorage.showFavouriteNumber();
+    console.log(`Updated Value ${updatedValue}`);
+}
+
+async function verify(_contractAddress, _args) {
+    console.log("Verifying Contract............");
+    /////////////////////////////
+    try {
+        await run("verify:verify", {
+            address: _contractAddress,
+            constructorArguments: _args,
+        });
+    } catch (error) {
+        if (error.message.toLowerCase().includes("already verified")) {
+            console.log("Already Verified");
+        } else {
+            console.log(error);
+        }
+    }
+}
 
 main().catch((error) => {
     console.error(error);
