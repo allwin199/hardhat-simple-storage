c21d0a768701231a6a9b2be1357813a96afdf3b2
worksofallwin <princeallwin199@gmail.com>
2023-07-18 14:59:02 +0530

    Complete deploy scripts for testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
+
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        sepolia: {
+            url: SEPOLIA_RPC_URL,
+            accounts: [SEPOLIA_PRIVATE_KEY],
+            chainId: 11155111
+        }
+    },
     solidity: "0.8.18",
 };
+
+//yarn hardhat run scripts/deploy.js --network sepolia
